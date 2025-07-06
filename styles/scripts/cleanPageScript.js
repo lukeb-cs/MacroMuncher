95d72ff8dbfc7c1aa4eb8b387d17272b178ca017
Luke B <luke.brenn04@gmail.com>
2025-07-06 11:48:36 -0400

    Delete styles/scripts directory

--- styles/scripts/cleanPageScript.js
+++
@@ -1,5 +0,0 @@
-// When button is pressed, clear all local storage items and reload the page
-document.getElementById("clean").addEventListener("click", function (event) {
-    localStorage.clear();
-    location.reload();
-});
